db55acdb2f0d454dab2803904f1c77db75663eed
Yuval Cohen <yuvalc11499@gmail.com>
2025-01-06 16:51:50 +0200

    Build Message schema and /api/message/users endpoint using getUsersForSidebar

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config()
@@ -14,7 +15,8 @@ const { PORT } = process.env
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
