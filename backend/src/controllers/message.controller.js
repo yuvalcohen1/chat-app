398e422b900a97d84a8daf1a6e7e4e7318291597
Yuval Cohen <yuvalc11499@gmail.com>
2025-01-06 18:41:21 +0200

    build sendMessages

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -28,6 +28,36 @@ export const getMessages = async (req, res) => {
         res.status(200).json(messages);
     } catch (error) {
         console.error("Error in getMessages controller: ", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        // todo: realtime functionality goes here => socket.io
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.error("Error in sendMessage controller: ", error.message);
         res.status(500).json({ error: "Internal Server Error" })
     }
 }
