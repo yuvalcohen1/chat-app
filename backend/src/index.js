a6c146a716d5682112433068ce59b673674d8917
Yuval Cohen <yuvalc11499@gmail.com>
2025-01-08 14:20:56 +0200

    Add cors midlleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-const { PORT } = process.env
+const { PORT } = process.env;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes)
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB()
-})
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
